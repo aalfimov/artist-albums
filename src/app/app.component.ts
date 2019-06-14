cb03b72aebb1b250caef1af52338e557a8cfa24f
o_alfimov <steamcat089@gmail.com>
2019-06-14 16:43:34 +0300

    deezer work with interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {SearchService} from './search.service';
 import {ResultsListItem} from './interfaces/result-list-item.interface';
-import {forkJoin, of} from 'rxjs';
-import {catchError} from 'rxjs/operators';
-import {ProcessingResultsService} from './processing-results.service';
-import {DeezerResponse} from './interfaces/deezer-response.interface';
-import {ItunesResponse} from './interfaces/itunes-response.interfase';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +8,7 @@ import {ItunesResponse} from './interfaces/itunes-response.interfase';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private searchService: SearchService, private processingService: ProcessingResultsService) {
+  constructor(private searchService: SearchService) {
   }
 
   private artist = '';
@@ -24,32 +19,36 @@ export class AppComponent implements OnInit {
   }
 
   getSearch() {
-
-    const deezerSubscription = this.searchService.getSearchFromDeezer(this.artist).pipe(catchError(() => of({
-      data: [],
-      next: null,
-      total: 0
-    } as DeezerResponse)));
-    const itunesSubscription = this.searchService.getSearchFromItunes(this.artist).pipe(catchError(() => of({
-      resultCount: 0,
-      results: []
-    } as ItunesResponse)));
-
-    forkJoin([deezerSubscription, itunesSubscription])
-      .subscribe(response => {
-        const deezer = response[0];
-        const itunes = response[1];
-
-        const normalizedData = [
-          this.processingService.convertResultFromDeezer(deezer.data),
-          this.processingService.convertResultFromItunes(itunes.results)
-        ];
-
-        console.log(normalizedData);
-
-        this.mergedResult = this.processingService.mergeData(normalizedData[0], normalizedData[1]);
-
-        console.log(this.mergedResult);
-      });
+    this.mergedResult = this.searchService.getSearch(this.artist);
   }
+
+  // getSearch() {
+  //
+  //   const deezerSubscription = this.searchService.getSearchFromDeezer(this.artist).pipe(catchError(() => of({
+  //     data: [],
+  //     next: null,
+  //     total: 0
+  //   } as DeezerResponse)));
+  //   const itunesSubscription = this.searchService.getSearchFromItunes(this.artist).pipe(catchError(() => of({
+  //     resultCount: 0,
+  //     results: []
+  //   } as ItunesResponse)));
+  //
+  //   forkJoin([deezerSubscription, itunesSubscription])
+  //     .subscribe(response => {
+  //       const deezer = response[0];
+  //       const itunes = response[1];
+  //
+  //       const normalizedData = [
+  //         this.processingService.convertResultFromDeezer(deezer.data),
+  //         this.processingService.convertResultFromItunes(itunes.results)
+  //       ];
+  //
+  //       console.log(normalizedData);
+  //
+  //       this.mergedResult = this.processingService.mergeData(normalizedData[0], normalizedData[1]);
+  //
+  //       console.log(this.mergedResult);
+  //     });
+  // }
 }
--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,22 +1,59 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {DeezerResponse} from './interfaces/deezer-response.interface';
-import {Observable} from 'rxjs';
+import {forkJoin, Observable, of} from 'rxjs';
 import {ItunesResponse} from './interfaces/itunes-response.interfase';
+import {catchError} from 'rxjs/operators';
+import {ProcessingResultsService} from './processing-results.service';
+import {ResultsListItem} from './interfaces/result-list-item.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private processingService: ProcessingResultsService) {
   }
+
   readonly DEEZER_URL = 'https://api.deezer.com/search';
   readonly ITUNES_URL = 'https://itunes.apple.com/search';
+  private mergedResult: ResultsListItem[];
 
   getSearchFromDeezer(artist: string): Observable<DeezerResponse> {
     return this.http.jsonp<DeezerResponse>(`${this.DEEZER_URL}?q=${artist}&output=jsonp`, 'callback');
   }
+
   getSearchFromItunes(artist: string): Observable<ItunesResponse> {
     return this.http.jsonp<ItunesResponse>(`${this.ITUNES_URL}?term=${artist}`, 'callback');
+  }
+
+  getSearch(artist: string) {
+
+    const deezerSubscription = this.getSearchFromDeezer(artist).pipe(catchError(() => of({
+      data: [],
+      next: null,
+      total: 0
+    } as DeezerResponse)));
+    const itunesSubscription = this.getSearchFromItunes(artist).pipe(catchError(() => of({
+      resultCount: 0,
+      results: []
+    } as ItunesResponse)));
+
+    forkJoin([deezerSubscription, itunesSubscription])
+      .subscribe(response => {
+        const deezer = response[0];
+        const itunes = response[1];
+
+        const normalizedData = [
+          this.processingService.convertResultFromDeezer(deezer.data),
+          this.processingService.convertResultFromItunes(itunes.results)
+        ];
+
+        console.log(normalizedData);
+
+        this.mergedResult = this.processingService.mergeData(normalizedData[0], normalizedData[1]);
+
+        console.log(this.mergedResult);
+      });
+    return this.mergedResult;
   }
 }
