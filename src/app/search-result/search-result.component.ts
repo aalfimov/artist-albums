5ba155c02652a6b0fa813c3c44c312c6d595a410
o_alfimov <steamcat089@gmail.com>
2019-06-19 15:57:59 +0300

    deezer work with interface

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ResultsListItem} from '../interfaces/result-list-item.interface';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-search-result',
