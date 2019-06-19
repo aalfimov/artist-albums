601c2b45a97979dd29bbfd5af64edd0b9eb59dcd
o_alfimov <steamcat089@gmail.com>
2019-06-19 15:20:28 +0300

    interactive routing

---
+++ src/app/search-result/search-result-resolver.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {ResultsListItem} from '../interfaces/result-list-item.interface';
+import {ActivatedRouteSnapshot, Resolve} from '@angular/router';
+import {SearchService} from '../search.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchResultResolver implements Resolve<Observable<ResultsListItem[]>> {
+
+  constructor(private searchService: SearchService) { }
+
+  resolve(route: ActivatedRouteSnapshot): Observable<ResultsListItem[]> {
+    const searchName = route.paramMap.get('artist');
+    return this.searchService.getSearch(searchName);
+  }
+}
---
+++ src/app/search-result/search-result.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {ResultsListItem} from '../interfaces/result-list-item.interface';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.scss']
+})
+export class SearchResultComponent implements OnInit {
+
+  result: ResultsListItem[];
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.result = data.resultsList;
+    });
+  }
+
+  noData() {
+    return !this.result.length;
+  }
+}
